7dc052715eb5149030e81dc258c05617c992eb0e
Hossam Ragab <hossam.alasklany@gmail.com>
2023-02-16 07:29:59 +0200

    screensize, clicked fun done

--- src/context/ContextProvider.js
+++ src/context/ContextProvider.js
@@ -3,7 +3,7 @@ import React, { createContext, useContext, useState } from "react";
 const StateContext = createContext();
 
 const initialState = {
-    caht: false,
+    chat: false,
     cart: false,
     userProfile: false,
     notification: false
@@ -11,10 +11,20 @@ const initialState = {
 
 export const ContextProvider = ({ children }) => {
     const [ activeMenu, setActiveMenu ] = useState(true);
+    const [ isClicked, setIsClicked ] = useState(initialState);
+    const handleClick = (clicked) => {
+        setIsClicked({ ...initialState, [ clicked ]: true });
+    };
+    const [ screenSize, setScreenSize ] = useState(undefined);
     return (
         <StateContext.Provider value={ {
             activeMenu,
-            setActiveMenu
+            setActiveMenu,
+            isClicked,
+            setIsClicked,
+            handleClick,
+            screenSize,
+            setScreenSize
         } }>
             { children }
         </StateContext.Provider>
