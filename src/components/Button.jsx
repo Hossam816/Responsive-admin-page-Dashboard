bb5837057583ee89b10b509816dd6a2813017aa7
Hossam Ragab <hossam.alasklany@gmail.com>
2023-02-22 16:33:10 +0200

    modified

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import { useStateContext } from "../context/ContextProvider";
 
-const Button = ({ bgColor, color, size, text, borderRadius }) => {
+const Button = ({ bgColor, color, size, icon, borderRadius }) => {
+  const { setIsClicked, initialState } = useStateContext();
   return (
     <button
       type="button"
-      style={{ background: bgColor, color, borderRadius }}
-      className={`text-${size} p-3 hover:drop-shadow-xl`}
+      style={ { background: bgColor, color, borderRadius } }
+      onClick={ () => setIsClicked(initialState) }
+      className={ `text-${size} p-3 hover:drop-shadow-xl` }
     >
-      {text}
+      { icon }
     </button>
   );
 };
