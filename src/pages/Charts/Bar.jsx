5f570977f031ab209a999a21748dbac8cac29f98
Hossam Ragab <hossam.alasklany@gmail.com>
2023-02-22 16:34:40 +0200

    charts done

--- src/pages/Charts/Bar.jsx
+++ src/pages/Charts/Bar.jsx
@@ -1,7 +1,51 @@
 import React from "react";
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  Legend,
+  Category,
+  Tooltip,
+  ColumnSeries,
+  DataLabel,
+} from "@syncfusion/ej2-react-charts";
+
+import {
+  barCustomSeries,
+  barPrimaryXAxis,
+  barPrimaryYAxis,
+} from "../../data/dummy";
+import { useStateContext } from "../../context/ContextProvider";
+import { Header } from "../../components";
 
 const Bar = () => {
-  return <div>Bar</div>;
+  const { currentMode } = useStateContext();
+
+  return (
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <Header category="Area" title="Olympic Medal Counts - RIO" />
+      <ChartComponent
+        id="line-chart"
+        height="420px"
+        primaryXAxis={barPrimaryXAxis}
+        primaryYAxis={barPrimaryYAxis}
+        chartArea={{ border: { width: 0 } }}
+        tooltip={{ enable: true }}
+        background={currentMode === "Dark" ? "#3337E" : "#fff"}
+        legendSettings={{ background: "white" }}
+      >
+        <Inject
+          services={[ColumnSeries, Tooltip, Category, DataLabel, Legend]}
+        />
+        <SeriesCollectionDirective>
+          {barCustomSeries.map((item, index) => (
+            <SeriesDirective key={index} {...item} />
+          ))}
+        </SeriesCollectionDirective>
+      </ChartComponent>
+    </div>
+  );
 };
 
 export default Bar;
--- src/pages/Charts/Financial.jsx
+++ src/pages/Charts/Financial.jsx
@@ -1,7 +1,74 @@
 import React from "react";
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  HiloSeries,
+  Tooltip,
+  DateTime,
+  Zoom,
+  Logarithmic,
+  Crosshair,
+} from "@syncfusion/ej2-react-charts";
+
+import {
+  financialChartData,
+  FinancialPrimaryXAxis,
+  FinancialPrimaryYAxis,
+} from "../../data/dummy";
+import { useStateContext } from "../../context/ContextProvider";
+import { Header } from "../../components";
+
+const date1 = new Date("2017, 1, 1");
+
+function filterValue(value) {
+  if (value.x >= date1) {
+    return value.x, value.high, value.low;
+  }
+}
+const returnValue = financialChartData.filter(filterValue);
 
 const Financial = () => {
-  return <div>Financial</div>;
+  const { currentMode } = useStateContext();
+
+  return (
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <Header category="Financial" title="Financial" />
+      <ChartComponent
+        id="line-chart"
+        height="420px"
+        primaryXAxis={FinancialPrimaryXAxis}
+        primaryYAxis={FinancialPrimaryYAxis}
+        chartArea={{ border: { width: 0 } }}
+        tooltip={{ enable: true }}
+        background={currentMode === "Dark" ? "#3337E" : "#fff"}
+        legendSettings={{ background: "white" }}
+      >
+        <Inject
+          services={[
+            HiloSeries,
+            Tooltip,
+            DateTime,
+            Logarithmic,
+            Zoom,
+            Crosshair,
+          ]}
+        />
+        <SeriesCollectionDirective>
+          <SeriesDirective
+            dataSource={returnValue}
+            xName="x"
+            yName="low"
+            name="Apple Inc"
+            type="Hilo"
+            low="low"
+            high="high"
+          />
+        </SeriesCollectionDirective>
+      </ChartComponent>
+    </div>
+  );
 };
 
 export default Financial;
