de8ec9b0fe6a2250afeb0f45fecb338d291bc12e
Hossam Ragab <hossam.alasklany@gmail.com>
2023-02-22 14:03:05 +0200

    sidebar modified

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,7 +7,7 @@ import { links } from "../data/dummy";
 import { useStateContext } from "../context/ContextProvider";
 
 const Sidebar = () => {
-  const { activeMenu, setActiveMenu, screenSize } = useStateContext();
+  const { activeMenu, setActiveMenu, screenSize, currentColor } = useStateContext();
   const handleCloseSidebar = () => {
     if (activeMenu && screenSize <= 900) {
       setActiveMenu(false);
@@ -50,6 +50,7 @@ const Sidebar = () => {
                     to={ `/${link.name}` }
                     key={ link.name }
                     onClick={ handleCloseSidebar }
+                    style={ ({ isActive }) => ({ backgroundColor: isActive ? currentColor : '' }) }
                     className={ ({ isActive }) =>
                       isActive ? activeLink : normalLink
                     }
@@ -62,8 +63,10 @@ const Sidebar = () => {
             )) }
           </div>
         </>
-      ) }
-    </div>
+      )
+
+      }
+    </div >
   );
 };
 
