848aa08c9af947a654ac89f3004cc0c9432e84c9
Hossam Ragab <hossam.alasklany@gmail.com>
2023-02-22 16:33:30 +0200

    modified

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="mt-24">
+      <p className="dark:text-gray-200 text-gray-600 text center m-20">
+        @ 2023 All right reserved by Hoso.com
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
